b9ffe76f4e7a4f3286b5acef7faa6c95c895ac37
Rencheng Chua <rench00@gmail.com>
2016-10-04 17:43:35 +0800

    change file limit

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ApplicationRecord
 
   # Validates the size of an uploaded picture.
   def picture_size
-    if picture.size > 5.megabytes
+    if picture.size > 10.megabytes
       errors.add(:picture, "should be less than 5MB")
     end
   end
